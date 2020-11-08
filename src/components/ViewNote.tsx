493c0f63fa7c7541423a544388763dedd4781682
TheodoreChu <22967798+TheodoreChu@users.noreply.github.com>
2020-11-08 00:54:17 -0800

    chore(version): v1.1.0-alpha.6

--- src/components/ViewNote.tsx
+++ src/components/ViewNote.tsx
@@ -394,7 +394,7 @@ export default class ViewNote extends React.Component<any, ChildState> {
                               🙂
                             </span>
                             <br />
-                            You are using version <code>1.1.0-alpha.5</code>.
+                            You are using version <code>1.1.0-alpha.6</code>.
                           </p>,
                         ]}
                       </details>
