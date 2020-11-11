0a2a64b4d8f0700ee06ba51a344988a194fe564e
TheodoreChu <22967798+TheodoreChu@users.noreply.github.com>
2020-11-10 21:48:45 -0800

    chore(version): v1.1.0-alpha.12

--- src/components/ViewNote.tsx
+++ src/components/ViewNote.tsx
@@ -394,7 +394,7 @@ export default class ViewNote extends React.Component<any, ChildState> {
                         🙂
                       </span>
                       <br />
-                      You are using version <code>1.1.0-alpha.11</code>. The
+                      You are using version <code>1.1.0-alpha.12</code>. The
                       release notes are available on{' '}
                       <a
                         href="https://github.com/TheodoreChu/append-editor/releases"
