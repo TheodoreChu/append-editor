28e2236161cd3035dd3cd647b495ebfb5cf55da1
TheodoreChu <22967798+TheodoreChu@users.noreply.github.com>
2020-12-07 16:19:51 -0800

    feat: icons for menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -146,10 +146,70 @@ export default class Menu extends React.Component<MenuProps, MenuState> {
     // You can render any custom fallback UI
     return (
       <div id={'menu'}>
-        <button onClick={this.copyText}>Copy note text</button>
-        <button onClick={this.copyHtml}>Copy rendered HTML</button>
-        <button onClick={this.formatText}>Format markdown text</button>
-        <button onClick={this.uncheckBoxes}>Uncheck all checkboxes</button>
+        <button onClick={this.copyText}>
+          <svg
+            name="Copy icon"
+            role="img"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.66724 3.66626C1.66724 2.56169 2.56267 1.66626 3.66724 1.66626H11.3339C12.4385 1.66626 13.3339 2.56169 13.3339 3.66626V13.3329H3.66724C2.56267 13.3329 1.66724 12.4375 1.66724 11.3329V3.66626ZM16.3339 6.66626C17.4385 6.66626 18.3339 7.56169 18.3339 8.66626V16.3329C18.3339 17.4375 17.4385 18.3329 16.3339 18.3329H8.66724C7.56267 18.3329 6.66724 17.4375 6.66724 16.3329V14.9996H15.0006V6.66626H16.3339ZM3.3339 3.33293V11.6663H11.6672V3.33293H3.3339Z"
+              fill="var(--sn-stylekit-success-color)"
+            />
+          </svg>
+          <span className="menu-button-caption">Copy note text</span>
+        </button>
+        <button onClick={this.copyHtml}>
+          <svg
+            name="Copy icon"
+            role="img"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1.66724 3.66626C1.66724 2.56169 2.56267 1.66626 3.66724 1.66626H11.3339C12.4385 1.66626 13.3339 2.56169 13.3339 3.66626V13.3329H3.66724C2.56267 13.3329 1.66724 12.4375 1.66724 11.3329V3.66626ZM16.3339 6.66626C17.4385 6.66626 18.3339 7.56169 18.3339 8.66626V16.3329C18.3339 17.4375 17.4385 18.3329 16.3339 18.3329H8.66724C7.56267 18.3329 6.66724 17.4375 6.66724 16.3329V14.9996H15.0006V6.66626H16.3339ZM3.3339 3.33293V11.6663H11.6672V3.33293H3.3339Z"
+              fill="var(--sn-stylekit-success-color)"
+            />
+          </svg>
+          <span className="menu-button-caption">Copy rendered HTML</span>
+        </button>
+        <button onClick={this.formatText}>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.2583 5.86655C17.5833 5.54155 17.5833 4.99989 17.2583 4.69155L15.3083 2.74155C15 2.41655 14.4583 2.41655 14.1333 2.74155L12.6 4.26655L15.725 7.39155L17.2583 5.86655ZM2.5 14.3749V17.4999H5.625L14.8417 8.27489L11.7167 5.14989L2.5 14.3749Z"
+              fill="var(--sn-stylekit-warning-color)"
+            />
+          </svg>
+          <span className="menu-button-caption">Format markdown text</span>
+        </button>
+        <button onClick={this.uncheckBoxes}>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M17.2583 5.86655C17.5833 5.54155 17.5833 4.99989 17.2583 4.69155L15.3083 2.74155C15 2.41655 14.4583 2.41655 14.1333 2.74155L12.6 4.26655L15.725 7.39155L17.2583 5.86655ZM2.5 14.3749V17.4999H5.625L14.8417 8.27489L11.7167 5.14989L2.5 14.3749Z"
+              fill="var(--sn-stylekit-danger-color)"
+            />
+          </svg>
+          <span className="menu-button-caption">Uncheck all checkboxes</span>
+        </button>
         <div
           className={`notification ${
             this.state.displayMessage ? 'visible' : 'hidden'
